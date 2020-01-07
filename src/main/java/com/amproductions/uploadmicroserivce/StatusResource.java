2ad6c5067477c538751783e953e1d27d3460b82d
marko <marko.p.krajinovic@gmail.com>
2020-01-07 14:48:17 +0100

    Add health check

---
+++ src/main/java/com/amproductions/uploadmicroserivce/StatusResource.java
@@ -0,0 +1,27 @@
+package com.amproductions.uploadmicroserivce;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.List;
+
+@Path("status")
+public class StatusResource {
+
+    @GET
+    public Response getImage(){
+        return Response.status(Response.Status.OK).build();
+    }
+}
